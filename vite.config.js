6333ced8836ea341dd1c64b1cfd22837026c7640
JoeyScottschronce <joeyscottschronce@gmail.com>
2024-10-26 12:41:24 -0400

    update base config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-commerce_website_final_project",
+  base: "/shoppingreact",
   plugins: [react()],
 })
